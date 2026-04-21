3f4eb8ec6267ea90c688d33af61734e8c60e6c23
JK Nordstrand <joohannees@hotmail.se>
2026-04-21 15:15:42 +0200

    add simple CartBtn component with dynamic count

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,22 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import Products from "./Products";
+import CartBtn from './components/CartBtn';
+
+
 
 function App() {
+
+  const [cart, setCart] = useState([]);
+
   return (
     <div>
+       <CartBtn
+        count={cart.length}
+        onClick={() => {
+          console.log("Go to cart");
+        }}
+      />
       <Products />
     </div>
   );
