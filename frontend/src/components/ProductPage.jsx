0c48e07cc5830ae5f109ca53b2701bfafaadfa05
SanjaStosic <s.a.nj.i.c.a_94@hotmail.com>
2026-04-29 13:48:58 +0200

    Fix: product page scroll

--- frontend/src/components/ProductPage.jsx
+++ frontend/src/components/ProductPage.jsx
@@ -1,54 +1,40 @@
 import { useParams } from "react-router-dom";
-import { useState } from "react";
 
-function ProductPage({ products = [], onAddToCart}) {
-  const { id } = useParams();
 
-  const product = products.find(
-    (product) => product.firestoreId === id
-  );
+function ProductPage({ products = [], onAddToCart }) {
+    const { id } = useParams();
+
+    const product = products.find(
+        (product) => product.firestoreId === id
+    );
 
     if (!product) return <p>No product selected</p>
 
-    const images = product.images 
-    ? product.images 
-    : [product.image, product.image, product.image];
+    const images = product.images
+        ? product.images
+        : [product.image, product.image, product.image];
 
-    const [selectedImage, setSelectedImage] = useState(images[0]);
 
-    
     return (
         <section className="product-page">
             <div className="product-page-left">
-                <img
-                    className="product-page-main-image"
-                    src={selectedImage}
-                    alt={product.name}
-                />
-
-                <div className="product-page-thumbnails">
-                    {images.map((image, index) => (
-                        <img
-                            key={index}
-                            src={image}
-                            alt={`${product.name} ${index + 1}`}
-                            onClick={() => setSelectedImage(image)}
-                            style={{ cursor: "pointer", width: "80px" }}
-                        />
-                    ))}
-                </div>
+                {images.map((image, index) => (
+                    <img
+                        key={index}
+                        className="product-page-scroll-image"
+                        src={image}
+                        alt={`${product.name} ${index + 1}`}
+                    />
+                ))}
             </div>
 
             <div className="product-page-right">
                 <h1>{product.name}</h1>
-
                 <p>{product.description}</p>
-
                 <p>{product.price} kr</p>
-
-                <button className="add-to-cart-btn" type="button" onClick={() => onAddToCart?.(product)}>Add to Cart</button>
-
-
+                <button className="add-to-cart-btn" type="button" onClick={() => onAddToCart?.(product)}>
+                    Add to Cart
+                </button>
             </div>
         </section>
     )
